002cb54b7b85934b8c1ebf6a781b440211baff0c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-02-05 13:25:08 -0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Adam Victor Brandizzi
+#
+# This file is part of Liferay Builder.
+#
+# Liferay Builder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Liferay Builder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Inelegant.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Liferay Builder",
+    version="0.0.1-dev1",
+    author='Adam Victor Brandizzi',
+    author_email='adam@brandizzi.com.br',
+    description='A tool for helping building Liferay',
+    license='LGPLv3',
+    url='http://bitbucket.com/brandizzi/lfr-builder',
+
+    packages=find_packages()
+)
